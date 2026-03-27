487a9f04e6988a58da1c7abcb6e753721a49fe23
holaqz <khaytbaev.art@bk.ru>
2026-03-27 15:14:26 +0500

    xvc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Очищаем кэш
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
 # Запускаем миграции (force для production)
 php artisan migrate --force
 
